bb495f85afa24dd25ed818c8dbfad34d7138e28b
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2023-04-13 19:46:56 +0900

    add transparency

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,3 +1,9 @@
-return { -- this table overrides highlights in all themes
+return {
+  -- this table overrides highlights in all themes
   -- Normal = { bg = "#000000" },
+  Normal = { bg = none },
+  NormalNC = { bg = none },
+  CursorColumn = { bg = none },
+  CursorLine = { bg = none },
+  CursorLineNC = { bg = none },
 }
