ccf795a9d89eb17bf40c3935b5db6a88a5fc7888
Jonathan Trudeau <48639690+trudeaujt@users.noreply.github.com>
2023-05-04 15:41:18 +0900

    add denots

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,11 +1,13 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.completion.copilot-lua-cmp" },
-  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.pack.tailwindcss" },
   { import = "astrocommunity.pack.yaml" },
 }
